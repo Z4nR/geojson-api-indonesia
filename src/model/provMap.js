1df3e76891be724b603cba4dd03e5f21de86be0e
Zulham Ari <ari.comunity1@gmail.com>
2022-02-11 09:37:17 +0700

    fix model type

--- src/model/provMap.js
+++ src/model/provMap.js
@@ -8,32 +8,7 @@ const ProvSchema = new Schema({
       type: String,
       required: true,
     },
-    feature: [
-      {
-        type: String,
-        geometry: {
-          type: {
-            type: String,
-            enum: ["Polygon"],
-            required: true,
-          },
-          coordinates: {
-            type: [[[Number]]],
-            required: true,
-          },
-        },
-        properties: {
-          Code: Number,
-          Country: String,
-          Kind: String,
-          Name: String,
-          Parent: Number,
-          Source: String,
-          Year: Number,
-        },
-      },
-    ],
   },
 });
 
-module.exports = mongoose.model("ProvMap", ProvSchema);
+module.exports = mongoose.model("provlatlong", ProvSchema, "ProvLatLong");
