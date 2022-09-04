d1ea60086afc65cdf05b6f2ff3140ffababf84c0
Zulham Ari <ari.comunity1@gmail.com>
2022-09-05 01:21:37 +0700

    repair router bug

--- src/routes/api-router.js
+++ src/routes/api-router.js
@@ -9,13 +9,13 @@ const {
 const router = require("express-promise-router")();
 
 //Province
-router.route("get-province-all").get(getProvMap);
-router.route("get-prov-detail").get(getProvDetail);
-router.route("get-prov-onIsland").get(getProvByIsland);
+router.route("/get-province-all").get(getProvMap);
+router.route("/get-prov-detail").get(getProvDetail);
+router.route("/get-prov-onIsland").get(getProvByIsland);
 
 //City
-router.route("get-cities-all").get(getCityMap);
-router.route("get-cities-prov").get(getCityByProv);
-router.route("get-city-onIsland").get(getCityByIsland);
+router.route("/get-cities-all").get(getCityMap);
+router.route("/get-cities-prov").get(getCityByProv);
+router.route("/get-city-onIsland").get(getCityByIsland);
 
 module.exports = router;
