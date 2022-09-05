fc704594563e1e18915a0bbbb3ebda5087869dd5
Zulham Ari <ari.comunity1@gmail.com>
2022-09-06 01:38:25 +0700

    implementing express promise router

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -4,60 +4,36 @@ const provgeo = require("../model/provMap");
 
 module.exports = {
   getProvMap: async (req, res, next) => {
-    try {
-      const prov = await provgeo.find();
-      res.status(200).json(prov);
-    } catch (err) {
-      next(err);
-    }
+    const prov = await provgeo.find();
+    res.status(200).json(prov);
   },
 
   getProvByIsland: async (req, res, next) => {
     const { island } = req.query;
-    try {
-      const provIsland = await provgeo.find({ island });
-      res.status(200).json(provIsland);
-    } catch (err) {
-      next(err);
-    }
+    const provIsland = await provgeo.find({ island });
+    res.status(200).json(provIsland);
   },
 
   getProvDetail: async (req, res, next) => {
     const { prov_id } = req.query;
-    try {
-      const detailProv = await provDtl.find({ prov_id });
-      res.status(200).json(detailProv);
-    } catch (err) {
-      next(err);
-    }
+    const detailProv = await provDtl.find({ prov_id });
+    res.status(200).json(detailProv);
   },
 
   getCityMap: async (req, res, next) => {
-    try {
-      const city = await citygeo.find();
-      res.status(200).json(city);
-    } catch (err) {
-      next(err);
-    }
+    const city = await citygeo.find();
+    res.status(200).json(city);
   },
 
   getCityByProv: async (req, res, next) => {
     const { prov_id } = req.query;
-    try {
-      const city = await citygeo.find({ prov_id });
-      res.status(200).json(city);
-    } catch (err) {
-      next(err);
-    }
+    const city = await citygeo.find({ prov_id });
+    res.status(200).json(city);
   },
 
   getCityByIsland: async (req, res, next) => {
     const { island } = req.query;
-    try {
-      const cityIsland = await citygeo.find({ island });
-      res.status(200).json(cityIsland);
-    } catch (err) {
-      next(err);
-    }
+    const cityIsland = await citygeo.find({ island });
+    res.status(200).json(cityIsland);
   },
 };
