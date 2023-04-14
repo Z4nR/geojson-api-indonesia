824dec45b586e27506126afd05473251e8ad0618
Zulham Ari <ari.comunity1@gmail.com>
2023-04-14 23:09:10 +0700

    loadAll data

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -4,36 +4,36 @@ const provgeo = require("../model/provMap");
 
 module.exports = {
   getProvMap: async (_req, res, _next) => {
-    const prov = await provgeo.find().limit(3);
+    const prov = await provgeo.find();
     res.status(200).json(prov);
   },
 
   getProvByIsland: async (req, res, _next) => {
     const { island } = req.query;
-    const provIsland = await provgeo.find({ island }).limit(3);
+    const provIsland = await provgeo.find({ island });
     res.status(200).json(provIsland);
   },
 
   getProvDetail: async (req, res, _next) => {
     const { prov_id } = req.query;
-    const detailProv = await provDtl.find({ prov_id }).limit(3);
+    const detailProv = await provDtl.find({ prov_id });
     res.status(200).json(detailProv);
   },
 
   getCityMap: async (_req, res, _next) => {
-    const city = await citygeo.find().limit(3);
+    const city = await citygeo.find();
     res.status(200).json(city);
   },
 
   getCityByProv: async (req, res, _next) => {
     const { prov_id } = req.query;
-    const city = await citygeo.find({ prov_id }).limit(3);
+    const city = await citygeo.find({ prov_id });
     res.status(200).json(city);
   },
 
   getCityByIsland: async (req, res, _next) => {
     const { island } = req.query;
-    const cityIsland = await citygeo.find({ island }).limit(3);
+    const cityIsland = await citygeo.find({ island });
     res.status(200).json(cityIsland);
   },
 };
